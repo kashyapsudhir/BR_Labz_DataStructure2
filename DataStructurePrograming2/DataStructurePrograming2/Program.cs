c15f03878147bf906aa3bc41d3f5c75bd822b8c1
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-22 17:18:43 +0530

    [70 will be added first then 30 and then 56 to make 56 on top of the Stack]

--- DataStructurePrograming2/DataStructurePrograming2/Program.cs
+++ DataStructurePrograming2/DataStructurePrograming2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ExceptionServices;
 
 namespace DataStructurePrograming2
 {
@@ -7,6 +8,13 @@ namespace DataStructurePrograming2
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello data structure programing2 start");
+
+            Stack myStack = new Stack();
+            myStack.push(70);
+            myStack.push(30);
+            myStack.push(56);
+            myStack.peek();
+           
         }
     }
 }
---
+++ DataStructurePrograming2/DataStructurePrograming2/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePrograming2
+{
+    public class Stack
+    {
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int data) //Constructor
+            {
+                this.data = data;
+            }
+
+        }
+        public Node Top;
+
+        public void push(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.Top == null)
+            {
+                newNode.next = this.Top;
+            }
+            this.Top = newNode;
+            Console.WriteLine($" The new top element is : {data}");
+        }
+
+        public void peek()
+        {
+            Node node = this.Top;
+            Console.WriteLine($"The top element is : {node.data}");
+        }
+
+        
+
+
+    }
+}
